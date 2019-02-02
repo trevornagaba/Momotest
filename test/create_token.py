6275b63179ff20f6ed7acc413960f4285ed9d492
Trevor Nagaba <trevornagaba@gmail.com>
2019-02-02 18:20:34 +0000

    First stable test version

--- test/create_token.py
+++ test/create_token.py
@@ -1,5 +1,6 @@
-import httplib, urllib, base64, uuid,json
+import http.client, urllib, base64, uuid, json, requests
 
+primary_key = '98bc5ef1ae864360b66024e1facadf51'
 
 ## Create reference_id
 
@@ -12,13 +13,13 @@ headers = {
     # Request headers
     'X-Reference-Id': reference_id,
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '98bc5ef1ae864360b66024e1facadf51',
+    'Ocp-Apim-Subscription-Key': primary_key,
 }
-params = urllib.urlencode({})
+params = urllib.parse.urlencode({})
 body = json.dumps({
   "providerCallbackHost": "http://127.0.0.1:4040" })
 try:
-    conn = httplib.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
     conn.request("POST", "/v1_0/apiuser?%s" % params, body, headers)
     response = conn.getresponse()
     print(response.status)
@@ -30,18 +31,61 @@ except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 api_user = reference_id
 
+
+## Get user details
+
+headers = {
+    # Request headers
+    'X-Reference-Id': reference_id,
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': primary_key
+}
+params = urllib.parse.urlencode({
+})
+
+body = json.dumps({
+  "providerCallbackHost": "http://127.0.0.1:4040" })
+
+# body = json.dumps(  
+#     {
+#         "amount": "string",
+#         "currency": "string",
+#         "externalId": "string",
+#         "payer": {
+#             "partyIdType": "MSISDN",
+#             "partyId": "string"
+#         },
+#         "payerMessage": "string",
+#         "payeeNote": "string"
+#     }
+# )
+
+try:
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn.request("POST", "/v1_0/apiuser/ed4fc92e-1b00-485a-b840-9fe55681d5d0?%s" % params, body, headers)
+    response = conn.getresponse()
+    print(response.status)
+    print(response.reason)
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
 ## Create api token
+
 headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '98bc5ef1ae864360b66024e1facadf51'
+    'Ocp-Apim-Subscription-Key': primary_key
 }
-params = urllib.urlencode({
+params = urllib.parse.urlencode({
 })
 body = json.dumps({
   "providerCallbackHost": "http://127.0.0.1:4040" })
 try:
-    conn = httplib.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
     conn.request("POST", "/v1_0/apiuser/ed4fc92e-1b00-485a-b840-9fe55681d5d0/apikey?%s" % params, body, headers)
     response = conn.getresponse()
     print(response.status)
@@ -51,7 +95,10 @@ try:
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
-api_key = json.loads(data)["apiKey"]
+api_key = json.loads(data.decode('utf8'))["apiKey"]
+
+
+
 
 
 ## Create jwt token
@@ -59,17 +106,25 @@ api_key = json.loads(data)["apiKey"]
 print(api_user)
 print(api_key)
 
+# params = urllib.parse.urlencode({
+# })
+# r = requests.post('https://ericssonbasicapi2.azure-api.net/collection/token/?%s' % params , auth=(api_user, api_key), headers = { 'Ocp-Apim-Subscription-Key' : primary_key } )
+# print(r.content)
+
+
 api_user_and_key  = api_user+':'+api_key
-encoded = base64.b64encode(api_user_and_key)
+encoded = base64.b64encode(api_user_and_key.encode())
 headers = {
     # Request headers
-    'Authorization': 'Basic '+encoded,
-    'Ocp-Apim-Subscription-Key': '98bc5ef1ae864360b66024e1facadf51',
+    'Authorization': 'Basic '+encoded.decode(),
+    'Ocp-Apim-Subscription-Key': primary_key,
 }
-params = urllib.urlencode({
+params = urllib.parse.urlencode({
 })
+body = json.dumps({
+  "providerCallbackHost": "http://127.0.0.1:4040" })
 try:
-    conn = httplib.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
     conn.request("POST", "/collection/token/?%s" % params, "{body}", headers)
     response = conn.getresponse()
     print(response.status)
--- test/generate_jwt_token.py
+++ test/generate_jwt_token.py
@@ -3,21 +3,66 @@
 {"apiUser":"6f1992b0-f2a4-4b78-b323-de60732fb369"}
 ##
 
-import httplib, urllib, base64
-api_user = "79c24602608a404b8c0810e06a761c57"
-api_key = "6f1992b0-f2a4-4b78-b323-de60732fb369"
+import http.client, urllib, base64, json, uuid
+
+api_key = "79c24602608a404b8c0810e06a761c57"
+api_user = "6f1992b0-f2a4-4b78-b323-de60732fb369"
+primary_key = '98bc5ef1ae864360b66024e1facadf51'
 api_user_and_key  = api_user+':'+api_key
-encoded = base64.b64encode(api_user_and_key)
+
+encoded = base64.b64encode(api_user_and_key.encode())
 headers = {
     # Request headers
-    'Authorization': 'Basic '+encoded,
-    'Ocp-Apim-Subscription-Key': '98bc5ef1ae864360b66024e1facadf51',
+    'Authorization': 'Basic '+encoded.decode(),
+    'Ocp-Apim-Subscription-Key': primary_key,
 }
-params = urllib.urlencode({
+params = urllib.parse.urlencode({
+})
+body = json.dumps({
+  "providerCallbackHost": "http://127.0.0.1:4040" })
+try:
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn.request("POST", "/collection/token/?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    print(response.status)
+    print(response.reason)
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+access_token = json.loads(data.decode())["access_token"]
+
+
+# Request to pay
+
+
+token = access_token
+reference_id = str(uuid.uuid4())
+headers = {
+    # Request headers
+    'Authorization': 'Bearer '+ token,
+    # 'X-Callback-Url': "http://127.0.0.1:4040",
+    'X-Reference-Id': reference_id,
+    'X-Target-Environment': 'sandbox',
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': primary_key,
+}
+params = urllib.parse.urlencode({})
+body = json.dumps({
+  "amount": "5000",
+  "currency": "EUR",
+  "externalId": "12345",
+  "payer": {
+    "partyIdType": "MSISDN",
+    "partyId": "0780123456"
+  },
+  "payerMessage": "test message",
+  "payeeNote": "test note"
 })
 try:
-    conn = httplib.HTTPSConnection('ericssonbasicapi2.azure-api.net')
-    conn.request("POST", "/collection/token/?%s" % params, "{body}", headers)
+    conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+    conn.request("POST", "/collection/v1_0/requesttopay?%s" % params, body, headers)
     response = conn.getresponse()
     print(response.status)
     print(response.reason)
